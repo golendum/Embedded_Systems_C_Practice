7566d1bc355c05c5fd9251b5886e8ba82f839610
Michael Golendukhin <golendum@mcmaster.ca>
2026-04-29 14:18:06 -0400

    First practice file finished: Simple basics
    Also added .gitignore to exclude object and executable files

--- C_Practice/exercises/01_basics.c
+++ C_Practice/exercises/01_basics.c
@@ -11,6 +11,7 @@ void problem_1() {
     printf("--- Problem 1: Basic Output ---\n");
     int value = 42;
     // TODO: Print the value using printf
+    printf("value = %d\n", value);
     // Expected output: value = 42
 }
 
@@ -20,7 +21,9 @@ void problem_2() {
     printf("Enter an integer: ");
     int input;
     // TODO: Read integer from user
+    scanf("%d", &input);
     // TODO: Print "You entered: X"
+    printf("You entered: %d\n", input);
 }
 
 // Problem 3: Understand data types and sizes
@@ -29,7 +32,29 @@ void problem_3() {
     // TODO: Print the size of each type using sizeof()
     // Types to check: char, short, int, long, float, double
     // uint8_t, uint16_t, uint32_t, uint64_t
+
+    int char_size = sizeof(char);
+    int short_size = sizeof(short);
+    int int_size = sizeof(int);
+    int long_size = sizeof(long);
+    int float_size = sizeof(float);
+    int double_size = sizeof(double);
+    int uint8_t_size = sizeof(uint8_t);
+    int uint16_t_size = sizeof(uint16_t);
+    int uint32_t_size = sizeof(uint32_t);
+    int uint64_t_size = sizeof(uint64_t);
+
     // Expected format: "char size: X bytes"
+    printf("char size: %d bytes\n", char_size);
+    printf("short size: %d bytes\n", short_size);
+    printf("int size: %d bytes\n", int_size);
+    printf("long size: %d bytes\n", long_size);
+    printf("float size: %d bytes\n", float_size);
+    printf("double size: %d bytes\n", double_size);
+    printf("uint8_t size: %d bytes\n", uint8_t_size);
+    printf("uint16_t size: %d bytes\n", uint16_t_size);
+    printf("uint32_t size: %d bytes\n", uint32_t_size);
+    printf("uint64_t size: %d bytes\n", uint64_t_size);
 }
 
 // Problem 4: Simple arithmetic
@@ -37,11 +62,11 @@ void problem_4() {
     printf("\n--- Problem 4: Arithmetic Operations ---\n");
     int a = 15, b = 4;
     // TODO: Calculate and print:
-    // - a + b
-    // - a - b
-    // - a * b
-    // - a / b
-    // - a % b (modulo)
+    printf("%d\n", (- a + b)); // - a + b
+    printf("%d\n", (- a - b)); // - a - b
+    printf("%d\n", (- a * b)); // - a * b
+    printf("%d\n", (- a / b)); // - a / b
+    printf("%d\n", (- a % b)); // - a % b
 }
 
 int main() {
